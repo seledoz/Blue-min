4e8f7b8cfcf3bdbcf6a124d5eab3a894d3956714
seledoz <donavonpfeifer@hotmail.com>
2026-07-05 23:06:52 -0600

    Include console alert in build

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ cat \
   src/modules/auto-magic-shield.js \
   src/modules/auto-attack.js \
   src/modules/auto-attack-aoe.js \
+  src/modules/red-text-alert.js \
   src/modules/cave.js \
   src/modules/equip-ring.js \
   src/modules/auto-eat.js \
