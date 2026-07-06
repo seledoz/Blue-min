27539982f6a959c704dd6a3c403c25d446ec5f21
seledoz <donavonpfeifer@hotmail.com>
2026-07-05 23:43:18 -0600

    Stop loading cave combat pause module

--- pz-bot.js
+++ pz-bot.js
@@ -15,7 +15,6 @@
     "src/modules/auto-attack-aoe.js",
     "src/modules/red-text-alert.js",
     "src/modules/cave.js",
-    "src/modules/cave-combat-pause.js",
     "src/modules/equip-ring.js",
     "src/modules/auto-eat.js",
     "src/modules/auto-fishing.js",
