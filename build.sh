05363498fd2bb516ce6ec9da8fb5b8169fd42a40
seledoz <donavonpfeifer@hotmail.com>
2026-07-05 23:51:15 -0600

    Include cave smart path in build

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ cat \
   src/modules/auto-attack-aoe.js \
   src/modules/red-text-alert.js \
   src/modules/cave.js \
+  src/modules/cave-smart-path.js \
   src/modules/equip-ring.js \
   src/modules/auto-eat.js \
   src/modules/auto-fishing.js \
